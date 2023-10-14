5d86af88923d63fe75348c757a5f64bdf1b7e134
Chris Tortoioli <147536547+taichimethod@users.noreply.github.com>
2023-10-14 12:20:42 +0100

    Added a bunch of JS to the script file

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics(){
+    for (x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's study CSS");
+    } else if (randomTopic === "Git") {
+        console.log("Let's study Git");
+    } else if (randomTopic === "JavaScript"){
+        console.log("Let's study Javascript");
+    } else {
+        console.log("Try Again!");
+    }
+}
+console.log("Here are the topics we learned through prework");
 
-/*
-if (topic === "HTML") {
-    console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-    console.log("Let's study CSS");
-} else if (topic === "Git") {
-    console.log("Let's study Git");
-} else if (topic === "JavaScript"){
-    console.log("Let's study Javascript");
-} else {
-    console.log("Try Again!");
-}
-*/
+listTopics();
+console.log("Which toic should we study first?");
+selectTopic();
